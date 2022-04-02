174e45fe4a1e37de05b81e49db94547f9f54741f
Harshit Dhiman <hd706020@gmail.com>
2022-04-02 11:53:30 +0530

    updated student list, connected api and fetch data from it

--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-function StudentsCard(){
+import axios from "axios";
+
+function StudentsCard({user}){
+
     return (
-        <li>
-            <div>Here will be the card</div>
-        </li>
+        <div className=" m-4 space-y-1">
+
+                <div className="w-64 shadow-md overflow-hidden">
+                    <img className="rounded-md object-cover h-full w-full" src={user.picture.large} alt="" />
+                </div>
+
+                <div className="flex flex-col mt-2">
+                    <h1 className="font-medium">{user.name.title} {user.name.last}</h1>
+                    <h1 className="text-indigo-500 font-medium">{user.login.username}</h1>
+                </div>
+                
+                <div className="flex items-center space-x-4">
+                    <a href="https://in.linkedin.com/"><img src="https://img.icons8.com/material-rounded/24/000000/linkedin--v1.png"/></a>
+                    <a href="https://twitter.com/"><img src="https://img.icons8.com/android/24/000000/twitter.png"/></a>
+                </div>
+            
+            </div>
     );
 }
 export default StudentsCard;
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,18 +1,32 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react/cjs/react.production.min";
 import StudentsCard from "./StudentCard";
 
 function UserList({}){
+    
+    const [user , setUserData]= React.useState([])
+    
+    React.useEffect(()=>{
+        const promise = axios.get("https://randomuser.me/api/?results=30");
+        promise.then( (reponse)=>{
+        setUserData(reponse.data.results)
+    })
+    },[])
+
     return(
         <div className="pt-10">
             <h1 className="mb-5 text-xl font-semibold">Students list </h1>
-            <div className="  px-4 py-4 mt-2 bg-white flex justify-center items-center ">
-                <div className="w-full max-w-4xl space-y-8">
-                    <ul>
-                        <StudentsCard></StudentsCard>
-                    </ul>
+            <div className=" py-4 mt-2 bg-white flex justify-center items-center ">
+                <div className="w-full max-w-4xl ">  
+                        <div className="flex  flex-wrap justify-center items-center">
+                       {
+                           user.map((u)=>(<StudentsCard user={u} />))
+                       }
+                        </div>                       
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
 export default UserList;
