d499dd0597d3dc2d4e2e0d118fe9de7851783949
Harshit Dhiman <hd706020@gmail.com>
2022-04-22 18:04:45 +0530

    Added formik and validation to the profile

--- src/FormikInput.jsx
+++ src/FormikInput.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useField } from "formik";
 
-function FormikInput({}) {
+function FormikInput({ name, type }) {
+  const [field, meta] = useField(name);
   return (
     <div className="m-1">
       <input
-        type="text"
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+        value={field.value}
+        name={name}
+        type={type || " text "}
         className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 bg-gray-50 rounded-md"
       />
+      <div className="h-5 text-red-500">
+        {meta.error && meta.touched && meta.error}
+      </div>
     </div>
   );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,26 +1,54 @@
+import { Form, Formik } from "formik";
 import React from "react";
+import { object, date, number, string } from "yup";
 import DropDown from "./DropDown";
 import FormikInput from "./FormikInput";
 
 function Profile({ myProfileDetails }) {
+  const handelDropDown = (event) => {
+    console.log(event.target.value);
+  };
+  const onFormSubmit = (data) => {
+    console.log(data);
+  };
+  const validationSchema = object().shape({
+    email: string().required().email(),
+    phone_number: string().min(10),
+    first_name: string().required(),
+    last_name: string().required(),
+    year_of_passout: date().required(),
+    roll_number: number().integer(),
+  });
 
-    
-  const handelDropDown = (event)=>{
-       console.log(event.target.value)
-  }
+  const intialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    institution_name: "",
+    year_of_passout: "",
+    phone_number: "",
+    roll_number: "",
+    branch: "",
+    dob: "",
+  };
+
   return (
-    <form className="h-full  pt-10">
+    <div className="h-full  pt-10">
       <div className="p-4 rounded-md bg-white">
         <h1 className="font-medium text-lg"> Personal details</h1>
-        <div className="mt-5 border-t">
-          <dl>
+        <Formik
+          onSubmit={onFormSubmit}
+          initialValues={intialValues}
+          validationSchema={validationSchema}
+          className="mt-5 border-t">
+          <Form>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
               <dt className="text-sm gray-600 font-medium">
                 First Name
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"first_name"} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -29,7 +57,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"last_name"} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -38,16 +66,16 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"email"} type={"email"} />
               </dd>
             </div>
-            <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
+            <div className="p-5 border-b grid grid-cols-3 s items-center">
               <dt className="text-sm gray-600 font-medium">
                 Institution Name
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"institution_name"} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -56,7 +84,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="">
-                <FormikInput />
+                <FormikInput name={"year_of_passout"} type={Date} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -65,7 +93,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"phone_number"} type={Number} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -74,7 +102,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="">
-                <FormikInput />
+                <FormikInput name={"dob"} type={Date} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -92,7 +120,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"roll_number"} type={number} />
               </dd>
             </div>
             <div className="p-5 border-b grid grid-cols-3 gap-4 items-center">
@@ -101,7 +129,7 @@ function Profile({ myProfileDetails }) {
                 <span className="text-red-600">*</span>
               </dt>
               <dd className="col-span-2">
-                <FormikInput />
+                <FormikInput name={"branch"} />
               </dd>
             </div>
             <div className=" p-5 ">
@@ -109,10 +137,10 @@ function Profile({ myProfileDetails }) {
                 Update
               </button>
             </div>
-          </dl>
-        </div>
+          </Form>
+        </Formik>
       </div>
-    </form>
+    </div>
   );
 }
 export default Profile;
